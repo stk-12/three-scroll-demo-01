c82784239d20c98f881626d5b20d192aa6c14004
keisuke sato <corydoras121@gmail.com>
2023-01-22 15:21:08 +0900

    [update]スクロールアニメーション

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ import Lenis from '@studio-freight/lenis';
 
 class Main {
   constructor() {
-    this.size = {
+    this.viewport = {
       width: window.innerWidth,
       height: window.innerHeight
     };
@@ -18,7 +18,7 @@ class Main {
       alpha: true
     });
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(this.size.width, this.size.height);
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
 
     this.scene = new THREE.Scene();
     this.camera = null;
@@ -38,23 +38,32 @@ class Main {
       // infinite: false,
     });
 
+    
+    // scroll animation
+    this.animations = [];
+    this.scrollPercent = 0;
+
+
     this._init();
     this._update();
     this._addEvent();
+    this._addAnimations();
 
+
+    // lenis
     this._getScrollValue();
   }
 
   _setCamera() {
-    // this.camera = new THREE.PerspectiveCamera(45, this.size.width / this.size.height, 1, 100);
+    // this.camera = new THREE.PerspectiveCamera(45, this.viewport.width / this.viewport.height, 1, 100);
     // this.camera.position.set(0, 0, 5);
     // this.scene.add(this.camera);
 
     //ウインドウとWebGL座標を一致させる
     const fov = 45;
     const fovRadian = (fov / 2) * (Math.PI / 180); //視野角をラジアンに変換
-    const distance = (this.size.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
-    this.camera = new THREE.PerspectiveCamera(fov, this.size.width / this.size.height, 1, distance * 2);
+    const distance = (this.viewport.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
+    this.camera = new THREE.PerspectiveCamera(fov, this.viewport.width / this.viewport.height, 1, distance * 2);
     this.camera.position.z = distance;
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     this.scene.add(this.camera);
@@ -84,6 +93,55 @@ class Main {
     })
   }
 
+  _addAnimations() {
+    this.animations.push({
+      start: 0,
+      end: 40,
+      animation() {
+        this.camera.lookAt(this.mesh.position);
+        // this.camera.position.set();
+        // this.mesh.position.z += 0.1;
+        this.mesh.position.z = lerp(0, 500, scalePercent(this.scrollPercent, 0, 40));
+      },
+    });
+
+    this.animations.push({
+      start: 40,
+      end: 50,
+      animation() {
+        this.camera.lookAt(this.mesh.position);
+        this.mesh.rotation.y = lerp(0, radian(360), scalePercent(this.scrollPercent, 40, 50));
+      },
+    });
+
+    this.animations.push({
+      start: 50,
+      end: 100,
+      animation() {
+        this.camera.lookAt(this.mesh.position);
+        // this.mesh.rotation.y = lerp(0, radian(360), scalePercent(this.scrollPercent, 40, 50));
+        // this.camera.position.x = lerp(0, 300, scalePercent(this.scrollPercent, 50, 70));
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.01;
+      },
+    });
+  }
+
+  _startAnimations() {
+    this.animations.forEach((animation)=>{
+      if(this.scrollPercent >= animation.start && this.scrollPercent <= animation.end) {
+        animation.animation.call(this);
+      }
+    });
+  }
+
+  _onScroll() {
+    this.scrollPercent = (scrollY / (document.body.scrollHeight - this.viewport.height)) * 100; // 進捗率の算出
+    console.log(this.scrollPercent);
+  }
+
+
+
   _init() {
     this._setCamera();
     // this._setControlls();
@@ -94,9 +152,11 @@ class Main {
   _update(time) {
 
     this.lenis.raf(time);
+
+    this._startAnimations();
     
-    this.mesh.rotation.y += 0.01;
-    this.mesh.rotation.x += 0.01;
+    // this.mesh.rotation.y += 0.01;
+    // this.mesh.rotation.x += 0.01;
 
     //レンダリング
     this.renderer.render(this.scene, this.camera);
@@ -105,30 +165,42 @@ class Main {
   }
 
   _onResize() {
-    this.size = {
+    this.viewport = {
       width: window.innerWidth,
       height: window.innerHeight
     }
     // レンダラーのサイズを修正
-    this.renderer.setSize(this.size.width, this.size.height);
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
     // カメラのアスペクト比を修正
-    this.camera.aspect = this.size.width / this.size.height;
+    this.camera.aspect = this.viewport.width / this.viewport.height;
     this.camera.updateProjectionMatrix();
   }
 
   _addEvent() {
     window.addEventListener("resize", this._onResize.bind(this));
+
+    window.addEventListener('scroll', this._onScroll.bind(this));
   }
 }
 
 new Main();
 
 
+// 線形補間
+function lerp(x, y, a) {
+  return (1 - a) * x + a * y;
+}
+
+
+function scalePercent(scrollPercent, start, end) {
+  return (scrollPercent - start) / (end - start);
+}
+
 
 // ラジアンに変換
-// function radian(val) {
-//   return (val * Math.PI) / 180;
-// }
+function radian(val) {
+  return (val * Math.PI) / 180;
+}
 
 // ランダムな数
 // function random(min, max) {
